0b80e5d9f716c1b8050fb177638847a624e76c0f
hvoking <gus@ugeom.com>
2024-07-04 18:37:34 -0400

    commented out drag pdf

--- src/components/main/pdf/index.tsx
+++ src/components/main/pdf/index.tsx
@@ -10,9 +10,9 @@ import { usePdf } from '../context/filters/pdf';
 
 export const UserPdf = () => {
 	const { page1Ref, page2Ref, printDocument, activePdf, setActivePdf } = usePdf();
-	const draggableRef = useRef<any>(null);
-	const offsetY = useRef<any>(0);
-	const isDragging = useRef(false);
+	// const draggableRef = useRef<any>(null);
+	// const offsetY = useRef<any>(0);
+	// const isDragging = useRef(false);
 
 	let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -25,38 +25,40 @@ export const UserPdf = () => {
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
 	});
 
-	const handleStart = (e: any) => {
-		e.stopPropagation()
-		e.preventDefault()
-        offsetY.current = (e.clientY || e.touches[0].clientY) - draggableRef.current.getBoundingClientRect().top;
-        isDragging.current = true;
+	// const handleStart = (e: any) => {
+	// 	e.stopPropagation();
+	// 	e.preventDefault();
+    //     offsetY.current = (e.clientY || e.touches[0].clientY) - draggableRef.current.getBoundingClientRect().top;
+    //     isDragging.current = true;
 
-        document.addEventListener('mousemove', handleMove);
-        document.addEventListener('mouseup', handleEnd);
-        document.addEventListener('touchmove', handleMove);
-        document.addEventListener('touchend', handleEnd);
-    };
+    //     document.addEventListener('mousemove', handleMove);
+    //     document.addEventListener('mouseup', handleEnd);
+    //     document.addEventListener('touchmove', handleMove);
+    //     document.addEventListener('touchend', handleEnd);
+    // };
 
-    const handleMove = (e: any) => {
-        if (isDragging.current) {
-        	const clientY = e.clientY || e.touches[0].clientY;
-        	const offset = clientY - offsetY.current;
-            const newTop = offset < 60 ? 60 : offset;
-            if (newTop) {
-                requestAnimationFrame(() => {
-                    draggableRef.current.style.top = `${newTop}px`;
-                });
-            }
-        }
-    };
+    // const handleMove = (e: any) => {
+    // 	e.stopPropagation();
+    // 	e.preventDefault();
+    //     if (isDragging.current) {
+    //     	const clientY = e.clientY || e.touches[0].clientY;
+    //     	const offset = clientY - offsetY.current;
+    //         const newTop = offset < 60 ? 60 : offset;
+    //         if (newTop) {
+    //             requestAnimationFrame(() => {
+    //                 draggableRef.current.style.top = `${newTop}px`;
+    //             });
+    //         }
+    //     }
+    // };
 
-    const handleEnd = () => {
-        isDragging.current = false;
-        document.removeEventListener('mousemove', handleMove);
-        document.removeEventListener('mouseup', handleEnd);
-        document.removeEventListener('touchmove', handleMove);
-        document.removeEventListener('touchend', handleEnd);
-    };
+    // const handleEnd = () => {
+    //     isDragging.current = false;
+    //     document.removeEventListener('mousemove', handleMove);
+    //     document.removeEventListener('mouseup', handleEnd);
+    //     document.removeEventListener('touchmove', handleMove);
+    //     document.removeEventListener('touchend', handleEnd);
+    // };
 
 	return (
 		<>
@@ -65,9 +67,9 @@ export const UserPdf = () => {
 					className="user-pdf" 
 					onClick={() => setActivePdf(false)}
 					tabIndex={0}
-					ref={draggableRef} 
-					onMouseDown={handleStart} 
-					onTouchStart={handleStart}
+					// ref={draggableRef} 
+					// onMouseDown={handleStart} 
+					// onTouchStart={handleStart}
 				>
 					<Page1 page1Ref={page1Ref} setActivePdf={setActivePdf}/>	
 					<Page2 page2Ref={page2Ref} setActivePdf={setActivePdf} printDocument={printDocument}/>
